02195bd5bf7c7c1b308bbde77331d6256fa1a04c
Itai Levy <levyi.tk@gmail.com>
2023-09-06 15:16:11 +0300

    fixes

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -5,22 +5,6 @@ sudo apt-get install sshpass -y
 ssh-keygen -t rsa -N '' -f /home/vagrant/.ssh/id_rsa <<< y
 chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
 chown vagrant:vagrant /home/vagrant/.ssh/id_rsa.pub
-#chmod 764 /home/vagrant/.ssh/id_rsa
 chmod 764 /home/vagrant/.ssh/id_rsa.pub
-#chmod 777 /home/vagrant/.ssh/authorized_keys
-echo 'sshpass -p vagrant ssh-copy-id -o "StrictHostKeyChecking=no" -i /home/vagrant/.ssh/id_rsa.pub vagrant@192.168.60.11' > /home/vagrant/sshing.sh
-#echo 'ssh -o "StrictHostKeyChecking=no" -i /home/vagrant/.ssh/id_rsa vagrant@192.168.60.11 hostname' > /home/vagrant/sshing1.sh
-#sshpass -p vagrant ssh-copy-id -o "StrictHostKeyChecking=no"  -i  /home/vagrant/.ssh/id_rsa.pub vagrant@192.168.60.10
-chown vagrant:vagrant /home/vagrant/sshing.sh
-chmod 774 /home/vagrant/sshing.sh
-#chmod 774 /home/vagrant/sshing1.sh
-#chmod +x sshing.sh
-#eval(sleep 60 && /home/vagrant/sshing.sh) & disown;
 
 sudo sed -i '$a\    StrictHostKeyChecking no' /etc/ssh/ssh_config
-#
-#sudo service ssh restart
-
-
- #ssh -o "StrictHostKeyChecking=no" server2
-#sshpass -p vagrant ssh-copy-id -o "StrictHostKeyChecking=no"  -i  /home/vagrant/.ssh/id_rsa.pub vagrant@192.168.60.11
--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -32,7 +32,9 @@ sshpass -p vagrant ssh-copy-id -o "StrictHostKeyChecking=no"  -i  /home/vagrant/
 
 
 echo "+++++++++>>>>>Exacuting script sshing.sh in server1(192.168.60.10)"
-ssh -vvv -tt -o "StrictHostKeyChecking=no" -i /home/vagrant/.ssh/id_rsa vagrant@192.168.60.10 '/usr/bin/bash /home/vagrant/sshing.sh'
+#ssh -vvv -tt -o "StrictHostKeyChecking=no" -i /home/vagrant/.ssh/id_rsa vagrant@192.168.60.10 '/usr/bin/bash /home/vagrant/sshing.sh'
+ssh -vvv -tt -o "StrictHostKeyChecking=no" -i /home/vagrant/.ssh/id_rsa vagrant@192.168.60.10 \
+                'sshpass -p vagrant ssh-copy-id -o "StrictHostKeyChecking=no" -i /home/vagrant/.ssh/id_rsa.pub vagrant@192.168.60.11'
 #ssh -vvv -tt -o "StrictHostKeyChecking=no" -i /home/vagrant/.ssh/id_rsa vagrant@192.168.60.10 '/usr/bin/bash /home/vagrant/sshing1.sh'
 sudo sed -i '$a\    StrictHostKeyChecking no' /etc/ssh/ssh_config
 
